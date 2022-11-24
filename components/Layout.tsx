817e4ebca42494033ecb29419799bbb0145cd313
ryuuwiz <ryuuwiz@gmail.com>
2022-11-24 15:38:49 +0700

    feat: add layout & navbar components

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,24 @@
-const Layout = () => {
-  return;
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: IProps) => {
+  return (
+    <>
+      <Head>
+        <title>AnimeDB</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const Navbar = () => {
-  return;
+  return (
+    <div className="bg-indigo-600 text-white flex justify-between items-center p-4">
+      <h1 className="text-3xl">AnimeDB</h1>
+      <ul className="flex justify-between">
+        <li className="px-2">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="px-2">
+          <a href="https://github.com/ryuuwiz/animedb">GitHub</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
