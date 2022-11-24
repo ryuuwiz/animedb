20240c4dc25f6f88c5c24c712ea3399545742ace
ryuuwiz <ryuuwiz@gmail.com>
2022-11-24 15:45:16 +0700

    style: change navbar styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="bg-indigo-600 text-white flex justify-between items-center p-4">
-      <h1 className="text-3xl">AnimeDB</h1>
-      <ul className="flex justify-between">
-        <li className="px-2">
+      <h1 className="text-2xl font-bold">AnimeDB</h1>
+      <ul className="flex justify-between text-md text-indigo-100 font-medium">
+        <li className="px-2 hover:text-indigo-200">
           <Link href="/">Home</Link>
         </li>
-        <li className="px-2">
+        <li className="px-2 hover:text-indigo-200">
           <a href="https://github.com/ryuuwiz/animedb">GitHub</a>
         </li>
       </ul>
