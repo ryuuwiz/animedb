3e05875c043df2ef2e3b7ed6feb0fefac7922aba
ryuuwiz <ryuuwiz@gmail.com>
2022-11-25 13:11:17 +0700

    feat: add footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <></>;
+  return (
+    <div className="text-center text-sm mt-5 px-4 py-10">
+      <p className="font-medium">© Ryu K</p>
+      <p>
+        Thanks to all the sponsors, contributors, and friends who have supported
+        this project.
+      </p>
+      <p>v1.0</p>
+    </div>
+  );
 };
 
 export default Footer;
