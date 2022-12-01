f97af35659c563e9973a0bfbd06765c5b9c4813c
ryuuwiz <ryuuwiz@gmail.com>
2022-12-01 13:36:14 +0700

    style: change card size with media queries

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-const Card = () => {
+const Card = ({ anime }: any) => {
   return (
     <div className="border flex flex-col items-center p-4 m-3 rounded hover:border-indigo-600 hover:border-2 hover:transition-all hover:duration-700 hover:cursor-pointer">
       <Image
-        src="https://cdn.myanimelist.net/images/anime/1207/128768.jpg"
-        alt="Naruto Image"
+        src={anime.images.webp.image_url}
+        alt={anime.title}
         width={200}
         height={200}
         className="rounded"
       />
-      <p className="pt-2 text-xl font-medium">Naruto</p>
+      <p className="pt-2 text-xl font-medium">{anime.title}</p>
     </div>
   );
 };
